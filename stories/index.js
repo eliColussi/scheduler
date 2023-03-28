d30b24d296fa1fb46ab85778250c3e94e41306c4
eli colussi <elicolussi@elis-MacBook-Pro.local>
2023-03-28 11:26:01 -0700

    main index of project

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DayList from "components/DayList";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -21,4 +22,35 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ))
+  .add("Wednesday", () => (
+    <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
