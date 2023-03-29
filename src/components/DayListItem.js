7222fbc8ec931311cd026a9ae222c24d18fb827b
eli colussi <elicolussi@elis-MacBook-Pro.local>
2023-03-29 01:39:33 -0700

    pass props down to children

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const { spots } = props
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    } else if (spots > 1) {
+      return `${spots} spots remaining`
+    } else {
+      return `no spots remaining`
+    }
+  }
+
+  const remainingSpots = formatSpots(spots);
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{remainingSpots}</h3>
     </li>
   );
 }
