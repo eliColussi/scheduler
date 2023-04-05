44fa2c83b36e15307898e177798011ea32e024e4
eli colussi <elicolussi@elis-MacBook-Pro.local>
2023-04-05 12:23:22 -0700

    update functions and state variables

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,30 +1,26 @@
 import React from "react";
-import "components/DayListItem.scss";
 import classNames from "classnames";
 
+import "components/DayListItem.scss";
+
 export default function DayListItem(props) {
-  const { spots } = props
-  const formatSpots = (spots) => {
-    if (spots === 1) {
-      return `${spots} spot remaining`
-    } else if (spots > 1) {
-      return `${spots} spots remaining`
-    } else {
-      return `no spots remaining`
-    }
-  }
-
-  const remainingSpots = formatSpots(spots);
-
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
   });
-
+  function formatSpots(spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  }
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{remainingSpots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
